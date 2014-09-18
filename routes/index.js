6d4ad902f0e1cfe0cdb5980bd203ca93dcf5a318
Doug Walter <doug@kosmojo.com>
2014-09-17 20:39:16 -0700

    updated

--- routes/index.js
+++ routes/index.js
@@ -13,66 +13,74 @@ router.get('/', function(req, res) {
 router.get('/twitter', function(req, res) {
   res.render('twitter');
 });
+//
+//var Twit = require('twit');
+//
+//var T = new Twit({
+//  consumer_key: process.env.CONSUMER_KEY
+//  , consumer_secret: process.env.CONSUMER_SECRET
+//  , access_token: process.env.ACCESS_TOKEN
+//  , access_token_secret: process.env.ACCESS_TOKEN_SECRET
+//});
+////
+//require('shelljs/global');
+//
+//
+//
+//var stream = T.stream("statuses/filter", { track: '#weatherplease'});
+//
+//stream.on("tweet", function(tweet){
+//
+//    var data = exec("curl -s htpp://api.openweathermap.org/data/2.5/weather?q=" + twitter.text); //without the hashtag
+//    //JSON-ize data.output -->right now it is just a string ..needs to be an object.
+//    // jsonData.weather.description ---> the sky is clear.
+//    // I need to post that with the post
+//    //In the status --> @tweeter   weather    location    Json.weather.description. find the tweet.text tweet.who tweet.location
+//    //    mg.sendText('nick@prmatch.com', 'doug@kosmojo.com',
+//    //        tweet.text,
+//    //        tweet.text,
+//    //        function(err){
+//    //            if(err) console.log('Oh noses: ' + err);
+//    //            else    console.log('Success');
+//    //        }
+//    //    );
+//});
+//
+//
+//router.post('/twitter', function(req, res) {
+//  // req.body.tweet
+//  T.post('statuses/update', { status: req.body.tweet }, function(err, data, response) {
+//    console.log(data);
+//  });
+//  res.redirect("/twitter");
+//});
+//
+//router.post('/twitter-search', function(req,res){
+//    //T.post search, using req.body.query
+//    T.get('search/tweets', {q: req.body.query, count:10}, function(err,data,response){
+//     res.json(data);
+//    });
+//});
 
-var Twit = require('twit');
-
-var T = new Twit({
-  consumer_key: process.env.CONSUMER_KEY
-  , consumer_secret: process.env.CONSUMER_SECRET
-  , access_token: process.env.ACCESS_TOKEN
-  , access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
-
-require('shelljs/global');
 
 
+var schedule = require('node-schedule');
 
-var stream = T.stream("statuses/filter", { track: '#weatherplease'});
-
-stream.on("tweet", function(tweet){
-
-    var data = exec("curl -s htpp://api.openweathermap.org/data/2.5/weather?q=" + twitter.text); //without the hashtag
-    //JSON-ize data.output -->right now it is just a string ..needs to be an object.
-    // jsonData.weather.description ---> the sky is clear.
-    // I need to post that with the post
-    //In the status --> @tweeter   weather    location    description. find the tweet.text tweet.who tweet.location
-//    mg.sendText('nick@prmatch.com', 'doug@kosmojo.com',
-//        tweet.text,
-//        tweet.text,
-//        function(err){
-//            if(err) console.log('Oh noses: ' + err);
-//            else    console.log('Success');
-//        }
-//    );
-});
-
-
-router.post('/twitter', function(req, res) {
-  // req.body.tweet
-  T.post('statuses/update', { status: req.body.tweet }, function(err, data, response) {
-    console.log(data);
-  });
-  res.redirect("/twitter");
-});
-
-router.post('/twitter-search', function(req,res){
-    //T.post search, using req.body.query
-    T.get('search/tweets', {q: req.body.query, count:10}, function(err,data,response){
-     res.json(data);
-    });
+var newScheduler = schedule.scheduleJob('* * * * *', function(){
+    console.log('The answer to life, the universe, and everything!');
 });
 
 var Mailgun = require('mailgun').Mailgun;
 
 var mg = new Mailgun(process.env.MG_KEY);
-//mg.sendText('nick@prmatch.com', 'doug@kosmojo.com',
-//    'this is the subject',
-//    'this is the text',
-//    function(err){
-//        if(err) console.log('Oh noses: ' + err);
-//        else    console.log('Success');
-//    }
-//);
+mg.sendText('cronCron@cron.com', 'doug@kosmojo.com',
+    newScheduler,
+    newScheduler,
+    function(err){
+        if(err) console.log('Oh noses: ' + err);
+        else    console.log('Success');
+    }
+);
 
 
 
